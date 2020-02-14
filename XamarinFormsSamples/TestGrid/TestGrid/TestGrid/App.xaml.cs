1a6fa615333456d66d04b526a301c98737aff525
DiegoJavierJ <49220228+DiegoJavierJ@users.noreply.github.com>
2020-02-13 23:37:39 -0400

    First

---
+++ XamarinFormsSamples/TestGrid/TestGrid/TestGrid/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestGrid
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new NavigationPage(new LoginPage());
+            MainPage = new DiscoveryPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ XamarinFormsSamples/TestGrid/TestGrid/TestGrid/Bookmark.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestGrid
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Bookmark : ContentPage
+    {
+        public Bookmark()
+        {
+            InitializeComponent();
+        }
+    }
+}
